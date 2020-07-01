ce5182dcae8a11358d18ca8c9545ad63003dccec
justanotherjoke <david.facsko@gmail.com>
2020-07-01 15:02:25 +0200

    Add ability to create new articles from front-end

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,4 +9,22 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def new
+
+  end
+
+  def create
+    #render plain: params[:article]  #ez is egyfajta debug, igy a bongeszoben jelenik meg az article tartalma
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    #render plain: @article.inspect
+    @article.save
+    # 'rails routes --expanded' parancs hatasara latjuk, hogy a show route-nal a prefix 'article'
+    # ahhoz, hogy ezt hasznalhassuk a kodban, akkor hozza kell rakni a '_path' -t
+    #redirect_to article_path(@article)
+    # shortcut:
+    redirect_to @article
+    # ha az osszes article-t kilistazo oldalra akarjuk iranyitani:
+    # redirect_to :articles
+  end
+
 end
