ccda153c8bbde00aa636202c9e7425b2467b91a3
justanotherjoke <david.facsko@gmail.com>
2020-07-01 12:32:06 +0200

    add articles listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,12 @@
 class ArticlesController < ApplicationController
   
   def show
-    byebug    # barhova irjuk a kodban, megszakitja a futast es ad egy debugger console-t (lasd Gemfile leiras)
+    #byebug    # barhova irjuk a kodban, megszakitja a futast es ad egy debugger console-t (lasd Gemfile leiras)
     @article = Article.find(params[:id])
+  end
+
+  def index
+    @articles = Article.all
   end
 
 end
