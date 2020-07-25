470f17cced32fe09901fd7d8d2fbbbb87c29ad4f
justanotherjoke <david.facsko@gmail.com>
2020-07-25 17:44:01 +0200

    Style validation and flash messages

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,10 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# get rid of the field_with_errors setting that rails is adding to an input field 
+# which is hit a validation error (every time you update environment rb or any kind of your
+# configuration file it's a good idea to restart your server)
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+	html_tag.html_safe
+end
