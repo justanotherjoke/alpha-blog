3e055c68efa23f7d334c419d7ada6c8783528887
justanotherjoke <david.facsko@gmail.com>
2020-10-11 22:50:47 +0200

    Complete categories create action and functional test

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,14 +1,30 @@
 class CategoriesController < ApplicationController
 
-    def new
+  def new
+    @category = Category.new
+  end
 
-    end
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category was created successfully."
+      redirect_to @category
+    else
+      render 'new'
+    end 
+  end
 
-    def index
+  def index
 
-    end
+  end
 
-    def show
+  def show
 
-    end
+  end
+
+  private
+  
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class CategoryTest < ActiveSupport::TestCase
 
   def setup
-    @category = Category.new(name: "Sports")
+    @category = Category.new(name: "Sports2")
   end
 
   test "category should be valid" do
